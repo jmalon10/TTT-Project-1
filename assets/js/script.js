4b8fbd9b367a21051f1673a7abe188c7911d7797
Jackie Maloney <jmaloney11277@gmail.com>
2024-09-12 12:13:40 -0400

    adds 2D array for character placement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,41 @@
 // DEPENDENCIES-----------------------------------------------------------------------------
-const playAgainButton = document.getElementById('play-again-btn');
 // DATA / STATE ----------------------------------------------------------------------------
 // FUNCTIONS -------------------------------------------------------------------------------
+// USER INTERACTIONS -----------------------------------------------------------------------
+// INITIALIZATIONS --------------------------------------------------------------------------
 
+// Pay again button functionality -----------------------------------------------------------
+const playAgainButton = document.getElementById('play-again-btn');
 function handlePlayAgainClick() {
     // Clear local storage
     localStorage.clear();
 
     // Refresh the page
     window.location.reload();
-}
-// USER INTERACTIONS -----------------------------------------------------------------------
-
+};
 playAgainButton.addEventListener('click', handlePlayAgainClick);
-
-// INITIALIZATIONS --------------------------------------------------------------------------
-
-
-
-
+//-------------------------------------------------------------------------------------------
+// Store X's and O's positions-------------------------------------------------------------------------------------------
+const twoDarray = [
+    [``, ``, ``],
+    [``, ``, ``],
+    [``, ``, ``],
+  ];
+  
+  const row = 0;
+  const col = 2;
+  
+  console.log("cell value", twoDarray[row][col]);
+  
+  cell.addEventListener("click", function(event) {
+    const cellClicked = event.target;
+  
+    const row = cellClicked.dataset.row;
+    const col = cellClicked.dataset.col;
+  
+    if (!twoDarray[row][col]) {
+      twoDarray[row][col] = playerCharacter;
+    }
+  })
 
 
