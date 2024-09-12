5c71d628b53e209f5c0a4843a31264caacf9a9a4
Jiju <178104698+jiju-codes@users.noreply.github.com>
2024-09-12 12:55:31 -0400

    Add function to hide the dropdown menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,18 @@ const playAgainButton = document.getElementById('play-again-btn');
 let currentPlayer = null; // No player is initially selected
 // FUNCTIONS -------------------------------------------------------------------------------
 
+// function to hide dropdown menu once player selects choice between Player X and Player O
+
+function hideDropdown() {
+    const dropdownMenu = document.querySelector('.dropdown-menu.show');
+    if (dropdownMenu) {
+      const dropdown = bootstrap.Dropdown.getInstance(document.getElementById('player-selector'));
+      if (dropdown) {
+        dropdown.hide();
+      }
+    }
+  }
+
 function handlePlayAgainClick() {
     // Clear local storage
     localStorage.clear();
