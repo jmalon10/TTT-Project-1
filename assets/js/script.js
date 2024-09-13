ea320ff6c9c7c5dd90a3e413240d5f8114385cd5
Jackie Maloney <jmaloney11277@gmail.com>
2024-09-13 12:28:32 -0400

    bugfix in main- deletes add event listeners function, redundancy with marktile function

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,29 +78,35 @@ function markTile(tile) {
     tile.textContent = currentPlayer;
     twoDarray[row][col] = currentPlayer;
     updateLocalStorage();
-    currentPlayer = (currentPlayer === 'X') ? 'O' : 'X'; // Switch player
+    if (currentPlayer !== null) {
+      currentPlayer = (currentPlayer === 'X') ? 'O' : 'X'; // Switch player
+  }
   }
 }
 
 // Add event listeners to each cell
-function setupCellEventListeners() {
-  const cells = document.querySelectorAll('.tile');
-  cells.forEach(cell => {
-    cell.addEventListener("click", function(event) {
-      const cellClicked = event.target;
-      const row = parseInt(cellClicked.dataset.row, 10);
-      const col = parseInt(cellClicked.dataset.column, 10);
+// function setupCellEventListeners() {
+//   const cells = document.querySelectorAll('.tile');
+//   cells.forEach(cell => {
+//     cell.addEventListener("click", function(event) {
+//       const cellClicked = event.target;
+//       const row = parseInt(cellClicked.dataset.row, 10);
+//       const col = parseInt(cellClicked.dataset.column, 10);
 
-      if (!cellClicked.textContent && twoDarray[row][col] === '') { // Check if cell is empty
-        const playerCharacter = currentPlayer; 
-        cellClicked.textContent = playerCharacter;
-        twoDarray[row][col] = playerCharacter; 
-        updateLocalStorage();
-        currentPlayer = (currentPlayer === 'X') ? 'O' : 'X'; // Switch player
-      }
-    });
-  });
-}
+//       if (!cellClicked.textContent && twoDarray[row][col] === '') { // Check if cell is empty
+//         const playerCharacter = currentPlayer; 
+//         cellClicked.textContent = playerCharacter;
+//         twoDarray[row][col] = playerCharacter; 
+//         updateLocalStorage();
+    
+//         // Switch player only if currentPlayer is not null
+//         if (currentPlayer !== null) {
+//             currentPlayer = (currentPlayer === 'X') ? 'O' : 'X'; // Switch player
+//         }
+//     }  
+//     });
+//   });
+// }
 
 // INITIALIZATIONS --------------------------------------------------------------------------
 
