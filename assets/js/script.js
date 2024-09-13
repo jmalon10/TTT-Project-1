f70534a36c0c92d97ab03a376298b9d3c18f492d
Jackie Maloney <jmaloney11277@gmail.com>
2024-09-13 15:20:46 -0400

    pseudo code checWin function

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,7 +83,14 @@ function markTile(tile) {
   }
   }
 }
-
+// FUNTION FOR CHECKING WINS
+// pull array from local storage -- twoDarray is a global varibale that already pulls the data from local storage
+  // if in the existing array...
+    // rows 0-2 all the same value-- horizontal win
+    // columns 0-2 all the same value-- vertical win
+    // diagonal ([0,0][1,1] [2,2])--- diagonal win
+    // anti diagonal ([0,2] [1,1] [2,0])-- antidiagonal win 
+    // call function when the new data saved in array
 
 // INITIALIZATIONS --------------------------------------------------------------------------
 
